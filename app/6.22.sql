2699094b846fd9c7f9a93f222696ac9d1bb698d8
xiazhongl <1814847542@qq.com>
2017-06-22 22:20:32 +0800

    6.22

---
+++ app/6.22.sql
@@ -0,0 +1,30 @@
+SELECT `gradeName` FROM `grade` (
+  WHERE 
+
+);
+
+INSERT INTO `hos_house`(hmid)
+
+CREATE TABLE student2(
+ id  INT(4),
+ namea CHAR(5),
+ age INT(3),
+ Home VARCHAR(50)
+ 
+);
+
+ALTER TABLE `student2` ADD CONSTRAINT `pk_atudent2` PRIMARY KEY `student2`(`id`);
+
+INSERT INTO `student2`(`namea`,`age`,`Home`) VALUES 
+  ('小四',16,'佛山'),
+  ('小五',12,'广州'),
+  ('小刘',13,'珠海'),
+  ('小七',17,'岛民'),
+  ('小八',18,'白云'),
+  ('小没',15,'佛山'),
+  ('小六',13,'禅城');
+
+SELECT * FROM `student2` WHERE  `Home`='佛山' ;
+
+
+  
